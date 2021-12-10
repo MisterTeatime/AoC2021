8ba0b20cb68b7def41f7436b1e94dafe78b7e54f
MisterTeatime <j.teatime@gmail.com>
2021-12-11 00:07:33 +0100

    Added Stack declaration

--- src/Utils.kt
+++ src/Utils.kt
@@ -65,3 +65,15 @@ class Line(coords: List<String>) {
         return "(${start.x}, ${start.y}) -> (${end.x}, ${end.y})"
     }
 }
+
+class Stack<E>(vararg items: E) {
+    private val elements = items.toMutableList()
+
+    fun push(element: E) = elements.add(element)
+    fun peek(): E = elements.last()
+    fun pop(): E = elements.removeAt(elements.size-1)
+    fun isEmpty() = elements.isEmpty()
+    fun size() = elements.size
+
+    override fun toString() = "Stack(${elements.joinToString()})"
+}
