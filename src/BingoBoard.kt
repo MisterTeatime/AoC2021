381f0ecb916b579b7df8cee6f5f7ab9801732dc3
MisterTeatime <j.teatime@gmail.com>
2021-12-28 02:03:11 +0100

    Solution for Day 4

--- src/BingoBoard.kt
+++ src/BingoBoard.kt
@@ -1,15 +1,29 @@
 class BingoBoard (input: List<String>){
     private var lines = mutableListOf<BingoLine>()
     private val processedInput = input.map{ it -> it.split(" ").filter{ s -> s.isNotEmpty()}}
+    var boardSum = input.joinToString(" ").split(" ").filterNot { it.isEmpty() }.sumOf { it.toInt() }
+
+    val bingo: Boolean
+        get() = lines.any { it.complete }
 
     init {
         for (l in processedInput) {
-            lines.add(BingoLine(l))
+            lines.add(BingoLine(l)) //add each horizontal line to the list of bingo lines
         }
 
         for (i in 0..4) {
-            lines.add(BingoLine(processedInput.map { it[i] }))
+            lines.add(BingoLine(processedInput.map { it[i] })) //add each vertical line to the list of bingo lines
         }
+    }
+
+    /****
+     * checks, if the called number is on the board.
+     * Each line checks the called number. On a hit deduct to called number from the board sum
+     */
+    fun checkNumber(num: Int) {
+        val check = lines.map { it.check(num) }.filter { it }
+        if (check.isNotEmpty())
+            boardSum -= num
     }
 
     override fun toString(): String {
@@ -26,9 +40,15 @@ class BingoLine (input: List<String>) {
         this.completed = 0
     }
 
-    fun isComplete(): Boolean = completed == 5
-    fun check(number: String) {
-        if (line.contains(number.toInt())) completed++
+    val complete: Boolean
+        get() = completed == 5
+
+    fun check(number: Int): Boolean {
+        if (line.contains(number)) {
+            completed++
+            return true
+        }
+        return false
     }
 
     override fun toString(): String {
--- src/Day04.kt
+++ src/Day04.kt
@@ -1,27 +1,45 @@
 fun main() {
     fun part1(input: List<String>): Int {
 
-//        val test = input.slice(2..6).map { it -> it.split(" ").filter { s -> s.isNotEmpty()}}
-//        println(test)
-//        for (i in 0..4) {
-//            println(test.map {it -> it[i]})
-//        }
         val numbers = input[0].split(",")
         val boards = input.drop(2).windowed(5,6).map{ BingoBoard(it) }
 
-        println(numbers)
-        println(boards)
+        for (num in numbers) {
+            boards.forEach { b ->
+                b.checkNumber(num.toInt()) //check the number
+                if (b.bingo) return b.boardSum * num.toInt() //board got bingo, so return its remaining sum times number called
+            }
+        }
 
-        return input.size
+        return 0
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+
+        val numbers = input[0].split(",")
+        val boards = input.drop(2).windowed(5,6).map{ BingoBoard(it) }.toMutableList()
+
+        for (num in numbers) {
+            val bingoed: MutableList<BingoBoard> = mutableListOf()
+            boards.forEach { b ->
+                b.checkNumber(num.toInt())
+                if (b.bingo) {
+                    when (boards.size) {
+                        1 -> return b.boardSum * num.toInt()
+                        else -> bingoed.add(b)
+                    }
+                }
+            }
+            if (bingoed.size > 0) boards.removeAll(bingoed)     //remove all boards, which bingoed
+        }
+
+        return 0
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 4512)
+    check(part2(testInput) == 1924)
 
     val input = readInput("Day04")
     println(part1(input))
